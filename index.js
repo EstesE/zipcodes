2819358374a0be7188cac478a519d1620e431235
mithrilhall <eric_estes@aspensquare.com>
2018-07-18 14:54:32 -0400

    Added elevation & timezone data.

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ let ZipCode = mongoose.model('zip', {
   defaultState: String,
   defaultRecordType: String,
   lastUpdated: Date,
+  location: Object,
+  county: String,
+  township: String,
+  timezone: Object,
   citiesList: Array,
   nonAcceptList: Array
 });
@@ -104,11 +108,54 @@ let queueWorkload = (zipCodes, fetchData, processResults) => {
       let spinner = ora(`Fetching data for ${zip}`).start();
       const form = new FormData();
       form.append('zip', zip);
-      fetch(config.url, { method: 'POST', body: form })
-        .then((res) => {
-          return res.json();
-        })
-        .then((json) => {
+
+      async function getData() {
+
+        let zipResults = await fetch(config.url, { method: 'POST', body: form })
+          .then((res) => {
+            return res.json();
+          })
+          .then((json) => {
+            return json;
+          })
+          .catch((err) => {
+            if (err) {
+              spinner.fail(`Error: ${err.message}`);
+            }
+          });
+
+        if (zipResults.resultStatus !== "INVALID-ZIP CODE") {
+          let options = {
+            select: `${zipResults.defaultCity}+${zipResults.defaultState}+${zipResults.zip5}`
+          };
+          let hasCoor = false;
+          let geoResults = await fetch(config.geocoding.baseApiURL + options.select + '&key=' + config.geocoding.key).then(response => {
+            hasCoor = true;
+            return response.json();
+          }).catch((err) => {
+            if (err) {
+              spinner.fail(`Error: ${err.message}`);
+            }
+          });
+
+          // Get elevation
+          let elevation = {};
+          let timezone = {};
+          if (geoResults && geoResults.results.length > 0) {
+            elevation = await fetch(`${config.elevation.baseApiURL}${geoResults.results[0].geometry.location.lat},${geoResults.results[0].geometry.location.lng}`).then(response => {
+              return response.json();
+            }).catch((err) => {
+              console.log(`Error: ${err}`);
+            });
+
+            // Get Timezone
+            timezone = await fetch(`${config.timezone.baseApiURL}${geoResults.results[0].geometry.location.lat},${geoResults.results[0].geometry.location.lng}&timestamp=${Math.floor(Date.now() / 1000)}`).then(response => {
+              return response.json();
+            }).catch((err) => {
+              console.log(`Error: ${err}`);
+            });
+          }
+
           spinner.succeed();
           const fs = require('fs');
           let fileContent = fs.readFileSync('zipcodes.json');
@@ -116,17 +163,70 @@ let queueWorkload = (zipCodes, fetchData, processResults) => {
           content.zipcodes.shift();
           let stream = fs.createWriteStream("zipcodes.json");
           let zips = '"' + content.zipcodes.join('","') + '"';
+          stream.write(`{ "zipcodes": [${zips}] }`);
 
+          if (hasCoor && geoResults && geoResults.results && geoResults.results[0] && geoResults.results[0].geometry && geoResults.results[0].geometry.location) {
+            zipResults.location = geoResults.results[0].geometry.location;
+            if (typeof (elevation) !== 'undefined') {
+              zipResults.location.elevation = elevation.results[0].elevation;
+            }
+            if (typeof (timezone) !== 'undefined') {
+              delete timezone.status;
+              zipResults.timezone = timezone;
+            }
+
+            function search(nameKey, myArray) {
+              function arraysEqual(arr1, arr2) {
+                if (arr1.length !== arr2.length)
+                  return false;
+                for (var i = arr1.length; i--;) {
+                  if (arr1[i] !== arr2[i])
+                    return false;
+                }
+                return true;
+              }
+
+              for (var i = 0; i < myArray.length; i++) {
+                let test = arraysEqual(nameKey, myArray[i].types);
+                if (test === true) {
+                  return myArray[i].long_name;
+                }
+              }
+            }
+
+            var county = search(["administrative_area_level_2", "political"], geoResults.results[0].address_components);
+            var township = search(["administrative_area_level_3", "political"], geoResults.results[0].address_components);
+
+            zipResults.county = county;
+            zipResults.township = township;
+          }
+          let myJson = zipResults;
+
+          stream.end(() => {
+            processResults({ zip: zip, results: myJson }, callback);
+          });
+          // 
+
+        } else {
+          // processResults({ zip: {}, results: {} }, callback);
+          spinner.succeed();
+          const fs = require('fs');
+          let fileContent = fs.readFileSync('zipcodes.json');
+          let content = JSON.parse(fileContent);
+          content.zipcodes.shift();
+          let stream = fs.createWriteStream("zipcodes.json");
+          let zips = '"' + content.zipcodes.join('","') + '"';
           stream.write(`{ "zipcodes": [${zips}] }`);
           stream.end(() => {
-            processResults({ zip: zip, results: json }, callback);
+            processResults({ zip: zip, results: {} }, callback);
           });
-        })
-        .catch((err) => {
-          if (err) {
-            spinner.fail(`Error: ${err.message}`);
-          }
-        });
+        }
+
+      }
+
+      getData();
+
+
     }, config.timeout);
   });
 };
@@ -154,13 +254,17 @@ let fetchData = function (zip, processResults, callback) {
 
 let processResults = (results, callback) => {
   results = results.results;
-  if (results && results.resultStatus !== 'INVALID-ZIP CODE') {
+  if (results && typeof (results.resultStatus) !== 'undefined' && results.resultStatus !== 'INVALID-ZIP CODE') {
     let zip = new ZipCode({
       zip5: results.zip5,
       defaultCity: results.defaultCity,
       defaultState: results.defaultState,
       defaultRecordType: results.defaultRecordType,
       lastUpdated: (new Date).getTime(),
+      location: results.location,
+      county: results.county,
+      township: results.township,
+      timezone: results.timezone,
       citiesList: results.citiesList,
       nonAcceptList: results.nonAcceptList,
     });
@@ -169,7 +273,7 @@ let processResults = (results, callback) => {
     delete z._id;
 
     let query = { zip5: z.zip5 };
-    ZipCode.findOneAndUpdate(query, { $set: { zip5: zip.zip5, defaultCity: zip.defaultCity, defaultState: zip.defaultState, defaultRecordType: zip.defaultRecordType, lastUpdated: zip.lastUpdated, citiesList: zip.citiesList, nonAcceptList: zip.nonAcceptList } }, { upsert: true, new: true }, function (err, doc) {
+    ZipCode.findOneAndUpdate(query, { $set: { zip5: zip.zip5, defaultCity: zip.defaultCity, defaultState: zip.defaultState, defaultRecordType: zip.defaultRecordType, lastUpdated: zip.lastUpdated, location: zip.location, county: zip.county, township: zip.township, timezone: zip.timezone, citiesList: zip.citiesList, nonAcceptList: zip.nonAcceptList } }, { upsert: true, new: true }, function (err, doc) {
       callback();
     });
 
